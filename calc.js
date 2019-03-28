fec3084374ae27f92d44a9c4092912d20dd2fb0f
Kefson <sm44200@zut.edu.pl>
2019-03-28 11:44:37 +0100

    ai_lab5

---
+++ calc.js
@@ -0,0 +1,20 @@
+const double = x => 2*x;
+ const init = (e,x) => {
+        function init(e,x){
+            console.log(typeof e);
+            console.info(double(x));
+console.log('container:',document.getElementById('container'));
+if(something) {
+const abc = {7,9};
+abc[8] = 8
+abc = 9;
+}
+
+
+}
+
+
+
+window.addEventListener('DOMconcentLoaded', ev =>
+init(e,5));
+
