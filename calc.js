c20b6a1e49bd293627862f747d23efc549c5af52
Kefson <sm44200@zut.edu.pl>
2019-04-04 11:46:45 +0200

    ai_lab6

--- calc.js
+++ calc.js
@@ -1,20 +1,44 @@
-const double = x => 2*x;
- const init = (e,x) => {
-        function init(e,x){
-            console.log(typeof e);
-            console.info(double(x));
-console.log('container:',document.getElementById('container'));
-if(something) {
-const abc = {7,9};
-abc[8] = 8
-abc = 9;
+const fields = [
+{txt:1, row:4, col:1 },
+    {txt:2, row:4, col:2 },
+    {txt:3, row:4, col:3 },
+    {txt:4, row:3, col:1 },
+    {txt:5, row:3, col:2 },
+    {txt:6, row:3, col:3 },
+    {txt:7, row:2, col:1 },
+    {txt:8, row:2, col:1 },
+    {txt:9, row:2, col:2 },
+    {txt:0, row:5, col:'1/span 2' },
+    {txt:'c', row:4, col:1 },
+    {txt:'+', row:4, col:1 },
+    {txt:'-', row:4, col:4 },
+    {txt:'=', row:5, col:3 },
+    {txt:'.', row:5, col:4 },
+    {txt:'Display', row:1, col:'1/span 4' }]
+
+const init = () => {
+
+    const container = document.getElementById('div');
+    container.id = 'container'
+
+    fields.forEach(each_array_element => {
+       const div = document.createElement('div');
+
+       div.textContent = each_array_element.text;
+       div.style.gridColumn =each_array_element.col;
+       div.style.gridRow = each_array_element.row;
+       if(each_array_element === 'Display') {
+        div.id='display';
+       } else {
+        div.addEventListener('click',ev => {
+ const d = document.getElementById('display')
+ d.textContent = ev.target.textContent;
+        });
+       }
+
+container.appendChild(div);
+    })
+    document.body.appendChild(container);
 }
 
-
-}
-
-
-
-window.addEventListener('DOMconcentLoaded', ev =>
-init(e,5));
-
+window.addEventListener('DOMContentLoaded', init);
